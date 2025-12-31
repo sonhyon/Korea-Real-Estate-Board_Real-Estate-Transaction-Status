eafdf4cad31c15af38989ac5d6e055e0a810134a
Hyeon <sonhyon86@gmail.com>
2026-01-01 01:46:30 +0900

    Delete 한국부동산원_부동산거래현황2getwd() setwd("C:/Users/User/OneDrive/문서/데이터분석/한국부동산원_부동산거래현황 directory

--- 한국부동산원_부동산거래현황2getwd() setwd("C:/Users/User/OneDrive/문서/데이터분석/한국부동산원_부동산거래현황/한국부동산원_부동산거래현황2.R
+++
@@ -1,6 +0,0 @@
-getwd()
-setwd("C:/Users/User/OneDrive/문서/데이터분석/한국부동산원_부동산거래현황/EXCEL2")
-
-RegionAptTrade <- read.csv("(연) 행정구역별 아파트거래현황_복사본.csv", fileEncoding = "UTF-8")
-head(RegionAptTrade)
-tail(RegionAptTrade)
